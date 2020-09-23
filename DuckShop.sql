f0bf92c4f5c8e1a692aee2e4edfc78b9408d2964
stefanjepsen <43956167+stefanjepsen@users.noreply.github.com>
2020-09-23 15:55:20 +0200

    Update DuckShop.sql

--- DuckShop.sql
+++ DuckShop.sql
@@ -23,10 +23,19 @@ CREATE TABLE Orders (
 );
 
 CREATE TABLE Products (
+    productID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     price DECIMAL(3, 2) NOT NULL,
     `name` VARCHAR(70) NOT NULL,
     color VARCHAR(50) NOT NULL,
     CONSTRAINT color CHECK (color NOT LIKE '%[^A-Z]%'),
     size VARCHAR(20) NOT NULL
     
+);
+
+CREATE TABLE OrdersHasProduct (
+    productID INT NOT NULL,
+    orderID INT NOT NULL,
+    CONSTRAINT PK_OrdersHasProduct PRIMARY KEY (productID, orderID),
+    FOREIGN KEY (productID) REFERENCES Products (productID),
+    FOREIGN KEY (orderID) REFERENCES Orders (orderID)
 );
